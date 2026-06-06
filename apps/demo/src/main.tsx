651704ad96075b1492ff923c4fee3d87bee1135a
gugol2 <4933016+gugol2@users.noreply.github.com>
2026-06-06 23:01:51 +0200

    add beacom plugin with the endpoint of my supabase url

--- apps/demo/src/main.tsx
+++ apps/demo/src/main.tsx
@@ -1,11 +1,16 @@
-import { RUMProvider } from '@rum-omo/react';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import { RUMProvider } from "@rum-omo/react";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RUMProvider config={{ debug: true }}>
+    <RUMProvider
+      config={{
+        debug: true,
+        endpoint: import.meta.env.VITE_VITALS_ENDPOINT,
+      }}
+    >
       <App />
     </RUMProvider>
   </React.StrictMode>,
